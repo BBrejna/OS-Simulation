edd7b2a355c5b88bb913e02846bdb1a1c2a5173e
BBrejna <bartosz.brejna@gmail.com>
2024-05-27 00:13:27 +0200

    Added rank column to output table

--- src/statistics/StatisticsHandler.java
+++ src/statistics/StatisticsHandler.java
@@ -13,20 +13,21 @@ public class StatisticsHandler {
     public static void printStatistics() {
         results.sort(Comparator.comparing(InstanceInfo::getScore));
 
-        String separator = "-------------------------------------------------------------------------------------------------------------------------------------%n";
+        String separator = "--------------------------------------------------------------------------------------------------------------------------------------------%n";
 
-        String format = "| %-6s | %-15s | %-8s | %-8s | %5s | %7s | %9s | %9s | %9s | %4s | %7s | %9s |%n";
+        String format = "| %-6s | %-15s | %-8s | %-8s | %5s | %7s | %9s | %9s | %9s | %4s | %7s | %9s | %4s |%n";
 
         System.out.printf(separator);
         System.out.printf(" Project statistics %n");
         System.out.printf(" Each possible combination %n");
 
         System.out.printf(separator);
-        System.out.printf(format, "CPU", "MEM", "RAM", "FRAMES", "PROC.", "CYCLES", "TURNAR.", "WAIT T.", "HDD ST.", "REJ.", "NO PAGE", "SCORE");
+        System.out.printf(format, "CPU", "MEM", "RAM", "FRAMES", "PROC.", "CYCLES", "TURNAR.", "WAIT T.", "HDD ST.", "REJ.", "NO PAGE", "SCORE", "RANK");
         System.out.printf(separator);
 
+        int rank = 1;
         for (InstanceInfo info : results) {
-            System.out.printf(format, info.cpuAlgo, info.memAlgo, info.ramAlgo, info.frameAlgo, info.processesNumber, info.cyclesDone, String.format("%.1f", info.avgTurnaround), String.format("%.1f", info.avgWaitTime), info.memStepsDone, info.memRejected, String.format("%.1f", info.ramPageErrors), String.format("%.1f", info.getScore()));
+            System.out.printf(format, info.cpuAlgo, info.memAlgo, info.ramAlgo, info.frameAlgo, info.processesNumber, info.cyclesDone, String.format("%.1f", info.avgTurnaround), String.format("%.1f", info.avgWaitTime), info.memStepsDone, info.memRejected, String.format("%.1f", info.ramPageErrors), String.format("%.1f", info.getScore()), rank++);
         }
 
         System.out.printf(separator);
