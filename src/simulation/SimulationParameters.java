7cecb4c058f9b5a81e4bf4d87d92cc81d5d3fd98
Aleksander <olojanic35@gmail.com>
2024-05-28 21:59:06 +0200

    Simulation parameteres

--- src/simulation/SimulationParameters.java
+++ src/simulation/SimulationParameters.java
@@ -5,7 +5,9 @@ public class SimulationParameters {
     public static int MAX_PROCESS_CPU_TIME = 30;
     public static int MAX_PROCESS_DELTA_TIME = 15;
     public static int MEM_SIZE = 50;
-    public static int RAM_SIZE = 3000;
+    public static int RAM_SIZE = 5000;
     public static int MAX_PROCESS_PAGE_NUMBER = 30;
     public static int RR_DELTA_TIME = 2;
+    public static int MANUAL_WINDOW = 10;
+    public static int ZONAL_WINDOW = 10;
 }
